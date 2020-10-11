5e9823518e677220f93144a4f1c50cd23459343e
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-10-11 11:29:23 -0600

    moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,24 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    # input is an array
+    # output is (?) the first element in the altered array (altered array is zeros moved to the right, or non zeros to the left of the zeros)
+    zeros = []
+    # go through array, slice the zero, append the sliced zero
+    for element in arr:
+        if element == 0:
+            arr.remove(element)
+            zeros.append(element)
+            print(zeros)
 
-    pass
+    for i in zeros:
+        arr.append(i)
+
+    return arr
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,14 +2,23 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
 
-    pass
+    # planning:
+    # sort,
+    # if n != n+1, return n
+
+    # if integer == arr[index+1] and arr[index+1] is not None:
+    # arr.remove(integer)
+    # arr.pop(index)
+    # return arr[-1]
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 1, 4, 4, 5, 5, 2, 2, 6, 3, 3, 9, 0, 0, 9]
 
     print(f"The odd-number-out is {single_number(arr)}")
